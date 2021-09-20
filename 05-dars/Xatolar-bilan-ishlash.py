7418bb5cf54eac5b433e4c6dfe4908c5c911d63b
Zokirkhon <78657264+Zokirkhon1002@users.noreply.github.com>
2021-09-20 11:20:31 +0900

    working with errors and github

---
+++ 05-dars/Xatolar-bilan-ishlash.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 20 10:54:13 2021
+
+@author: Zokirkhon
+"""
+
+#1
+# son = int(input("Juft son kiriting:\n>>> "))
+          
+# if son%2:
+#     print("Bu son juft emas.")
+# else:
+#     print("Rahmat!")
+
+
+#2
+# yosh = float(input("Yoshingiz nechida?\n>>> "))
+
+# if yosh<=4 or yosh>=60:
+#     narh = 0
+# elif yosh < 18:
+#     narh = 10000
+# else:
+#     narh = 20000
+
+# print(f"Chipta {narh} so'm")
+
+
+
+#3
+# x = float(input("Birinchi sonni kiriting:\n>>> "))
+# y = float(input("Ikkinchi sonni kiriting:\n>>> "))
+# if x==y:
+#     print(f"{x}={y}")
+# elif x<y:
+#     print(f"{x}<{y}")
+# else:
+#     print(f"{x}>{y}")
+
+
+
+
+#4
+# mahsulotlar = ['un', "yog'", "sovun", 'tuxum', 'piyoz',
+#                'kartoshka', 'olma', 'banan', 'uzum', 'qovun']
+
+# savat = []
+
+# for n in range(5):
+#     savat.append(input(f"Savatga {n+1}-mahsulotni qo'shing:\n>>> "))
+
+# if savat:
+#     for mahsulot in savat:
+#         if mahsulot in mahsulotlar:
+#             print(f"Do'konimizda {mahsulot} bor")
+#         else:
+#             print(f"Do'konimizda {mahsulot} yo'q")
+# else: 
+#     print("Savatingiz bo'sh")     
+
+
+
+
+#5
+# mahsulotlar = ['un', "yog'", "sovun", 'tuxum', 'piyoz',
+#                'kartoshka', 'olma', 'banan', 'uzum', 'qovun']
+
+
+# savat = []
+
+# for n in range(5):
+#     savat.append(input(f'Savatga {n+1}-mahsulotni qo\'shing:\n>>> '))
+
+# bor_mahsulotlar = []
+# mavjud_emas = []
+
+# for mahsulot in savat:
+#     if mahsulot in mahsulotlar:
+#         bor_mahsulotlar.append(mahsulot)
+#     else:
+#         mavjud_emas.append(mahsulot)
+
+
+# if mavjud_emas:
+#     print("Do'konimizda quyidagi mahsulotlar yo'q:")
+#     for mahsulot in mavjud_emas:
+#         print(mahsulot)
+# else:
+#   print("Siz so'ragan barcha mahsulotlar do'konimizda bor")
+
+
+
+
+#6
+# users = ['alisher1983','aziza','yasina' 'umar']
+
+# login = input("Yangi login tanlang:\n>>> ")
+
+# if login in users:
+#     print('Login band, yangi login tanlang!')
+# else:
+#     print("Xush kelibsiz!")
+    
+
+
+
